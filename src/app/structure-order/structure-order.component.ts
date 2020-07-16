17dcdf48529eb50c4f09b44443d2e74a858f555c
Ariel Perez <ariel@Ariels-MacBook-Pro.local>
2020-07-15 22:59:29 -0400

    Header appearance created.

---
+++ src/app/structure-order/structure-order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-structure-order',
+  templateUrl: './structure-order.component.html',
+  styleUrls: ['./structure-order.component.css']
+})
+export class StructureOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
