852edcb0d22d307e6f2e2f2b4cb1ead794c0581b
Ariel Perez <ariel@Ariels-MacBook-Pro.local>
2020-07-16 23:07:36 -0400

    Recipes added with class Recipe as a model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Arroz con pollo a la chorrera',
+      // tslint:disable-next-line: max-line-length
+      'Otro plato muy concurrido en las mesas de la isla es el arroz con pollo, es muy común servirlo acompañado de plátano frito o pan, se sirve bien caliente, y es plato principal por excelencia, he aquí la receta para que hagas de tus celebraciones un festín muy cubano',
+      'https://www.cocina-cubana.com/base/stock/Recipe/83-image/83-image_web.jpg'
+      ),
+      new Recipe(
+        'Ajiaco Cubano',
+        // tslint:disable-next-line: max-line-length
+        'El ajiaco es un platillo nacional que se gestó durante por lo menos trescientos años de historia. Se consolidó en comida nacional hasta después de las primeras guerras importantes en cuba. La receta comenzó siendo una especie de cocido en el que se agregaba a la olla toda clase de ingredientes de origen vegetal y animal procedentes de las tres culturas, la europea, la africana y la autóctona, que se amalgaman en la gastronomía cubana.',
+        'https://www.cocina-cubana.com/base/stock/Recipe/2-image/2-image_web.jpg'
+        )
+  ];
 
   constructor() { }
 
