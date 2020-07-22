9a4fcc8f3178c66b271662f810675b3e37a79555
Ariel Perez <ariel@Ariels-MacBook-Pro.local>
2020-07-21 22:27:01 -0400

    new ingredients added to shopping-list component & list created to be shown

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,6 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+  ingredients: Ingredient[] = [
+    new Ingredient('tazas de agua', 6),
+    new Ingredient('libra de carne de puerco cortada en trozos', 1),
+    new Ingredient('libra de polio cortado en trozos', 0.5),
+    new Ingredient('libra de ñame', 0.5),
+    new Ingredient('libra de boniato', 1),
+    new Ingredient('libra de yuca', 1),
+    new Ingredient('trozo de calabaza', 1),
+    new Ingredient('plátanos machos', 2),
+    new Ingredient('plátano verde', 1),
+    new Ingredient('libra de malanga', 1),
+    new Ingredient('taza de tomate molido', 1),
+    new Ingredient('ají grande', 1),
+    new Ingredient('dientes de ajo', 4),
+    new Ingredient('cebolla picada', 1)
+  ];
   constructor() { }
 
   ngOnInit(): void {
