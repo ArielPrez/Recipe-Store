a5316e274793b40b5a6dc666f1cab254473da2ca
Ariel Perez <ariel@Ariels-MacBook-Pro.local>
2020-09-14 07:10:21 -0400

    Building Dropdown Directive for a dropdown menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { StructureOrderComponent } from './structure-order/structure-order.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { StructureOrderComponent } from './structure-order/structure-order.compo
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    StructureOrderComponent
+    StructureOrderComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
