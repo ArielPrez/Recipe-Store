88054adaccae1aafaca5c0308ae0a507b2f8d43e
Ariel Perez <ariel@Ariels-MacBook-Pro.local>
2020-10-07 16:55:17 -0400

    Added the Shopping-List Service, using it for Pushing Data into the Array of Ingredients

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -10,9 +11,8 @@ export class ShoppingEditComponent implements OnInit {
 
   @ViewChild('nameInput', {static: false}) nameRef: ElementRef;
   @ViewChild('amountInput', {static: false}) amountRef: ElementRef;
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
   }
@@ -21,7 +21,7 @@ export class ShoppingEditComponent implements OnInit {
     const ingredtName = this.nameRef.nativeElement.value;
     const ingredtAmount = this.amountRef.nativeElement.value;
     const newIngredient = new Ingredient(ingredtName, ingredtAmount);
-    this.ingredientAdded.emit(newIngredient);
+    this.shoppingListService.addIngredient(newIngredient);
   }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,34 @@
+import { Injectable } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+@Injectable({providedIn: 'root'})
 export class ShoppingListService {
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    private ingredients: Ingredient[] = [
+        new Ingredient('tazas de agua', 6),
+        new Ingredient('libra de carne de puerco cortada en trozos', 1),
+        new Ingredient('libra de polio cortado en trozos', 0.5),
+        new Ingredient('libra de ñame', 0.5),
+        new Ingredient('libra de boniato', 1),
+        new Ingredient('libra de yuca', 1),
+        new Ingredient('trozo de calabaza', 1),
+        new Ingredient('plátanos machos', 2),
+        new Ingredient('plátano verde', 1),
+        new Ingredient('libra de malanga', 1),
+        new Ingredient('taza de tomate molido', 1),
+        new Ingredient('ají grande', 1),
+        new Ingredient('dientes de ajo', 4),
+        new Ingredient('cebolla picada', 1)
+      ];
 
+    getIngredients() {
+        // Return a copy of the Array ingredients, without parameters inside, the method returns all.
+        return this.ingredients.slice();
+    }
+    addIngredient(ingredient: Ingredient) {
+        // Save the new ingredient.
+        this.ingredients.push(ingredient);
+        // Emit a copy of the Array ingredients
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
 }
