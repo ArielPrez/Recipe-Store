b3b130e832e5963db68c4f0e5ce3c1e0d1c90530
Ariel Perez <ariel@Ariels-MacBook-Pro.local>
2020-10-30 20:41:02 -0400

    Project Cleanup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -19,7 +19,7 @@ export class RecipeListComponent implements OnInit {
     this.recipes = this.recipeService.getRecipes();
   }
 
-  onNewRecipe(){
+  onNewRecipe() {
     this.router.navigate(['new'], {relativeTo: this.route});
   }
 
