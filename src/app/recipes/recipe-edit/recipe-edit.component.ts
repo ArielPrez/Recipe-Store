05d83a2b68692f68e9dd397db9119c30e3d5423f
Ariel Perez <ariel@Ariels-MacBook-Pro.local>
2020-12-08 18:40:03 -0500

    TypeScript - Creating the Form For Editing Recipes

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
+import { RecipeService } from './../recipe.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -10,16 +12,38 @@ export class RecipeEditComponent implements OnInit {
 
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private recipeService: RecipeService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = params.id;
         this.editMode = params.id != null;
+        this.initForm();
       }
     );
+  }
+
+  private initForm() {
+    let recName = '';
+    let recImgPath = '';
+    let recDescrip = '';
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recName = recipe.name;
+      recImgPath = recipe.imagePath;
+      recDescrip = recipe.description;
+    }
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recName),
+      'imgPath': new FormControl(recImgPath),
+      'description': new FormControl(recDescrip)
+    });
   }
 
 }
