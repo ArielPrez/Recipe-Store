293778033cab72665fd34587efe2df1492ae27ad
Ariel Perez <ariel@Ariels-MacBook-Pro.local>
2020-12-15 23:15:26 -0500

    Using, Parameterizing and Chaining Multiple Pipes. Creating a Custom Pipe.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,8 +16,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { RecipeService } from './recipes/recipe.service';
+import { ShortenPipe } from './shared/shorten.pipe';
 
 @NgModule({
   declarations: [
@@ -32,7 +31,8 @@ import { RecipeService } from './recipes/recipe.service';
     StructureOrderComponent,
     DropdownDirective,
     RecipeStartComponent,
-    RecipeEditComponent
+    RecipeEditComponent,
+    ShortenPipe
   ],
   imports: [
     BrowserModule,
