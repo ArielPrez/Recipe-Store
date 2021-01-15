a21eccb5b2596f078ad59486ea63b4c48c7f22af
ArielPrez <arielpgmm@gmail.com>
2021-01-15 16:27:20 -0500

    Saving the Recipes in the Server(Firebase)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { DataStorageService } from './../shared/data-storage.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,10 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent {
   collapsed = true;
+
+  constructor(private dataStorage: DataStorageService) {}
+
+  onSaveData() {
+    this.dataStorage.storeRecipes();
+  }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,3 +1,4 @@
+import { RecipeService } from './../recipes/recipe.service';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
@@ -6,5 +7,17 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DataStorageService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private recipeService: RecipeService) { }
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put('https://recipe-store-book-default-rtdb.firebaseio.com/recipe.json', recipes)
+    .subscribe(
+        (response) => {
+          console.log(response);
+        }
+      );
+
+  }
 }
